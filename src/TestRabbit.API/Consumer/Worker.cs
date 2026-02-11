cdbf86f14b494ee14f7d8d4a394a9b82a224059c
p3t3r276 <p3t3r276@gmail.com>
2026-02-12 00:42:32 +0700

    Update

---
+++ src/TestRabbit.API/Consumer/Worker.cs
@@ -0,0 +1,24 @@
+using TestRabbit.API.Services;
+
+namespace TestRabbit.API.Consumer;
+
+public class Worker(ILogger<Worker> logger, RabbitMqConsumer consumer) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("Order Processing Worker started at: {time}", DateTimeOffset.UtcNow);
+
+        // Start consuming messages from RabbitMQ
+        await consumer.StartConsumingAsync();
+
+        logger.LogInformation("Worker is now listening for orders...");
+
+        // Keep the worker running
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+        }
+
+        logger.LogInformation("Order Processing Worker stopping at: {time}", DateTimeOffset.UtcNow);
+    }
+}
