61ca6983cdeb3947847424eceeba3bbdfc3da5ec
p3t3r276 <p3t3r276@gmail.com>
2026-02-12 21:33:35 +0700

    Seperating ptojects

---
+++ src/TestRabbit.Consumer/Program.cs
@@ -0,0 +1,7 @@
+using TestRabbit.Consumer;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ src/TestRabbit.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace TestRabbit.Shared;
+
+public class Class1
+{
+
+}
